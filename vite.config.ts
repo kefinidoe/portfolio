974c84546b696c1e7ab0a43ac32d66fd2deadd1e
kelvin kimani <kelvinmwangi1744@gmail.com>
2026-03-05 14:13:38 +0300

    Fix: Remove build artifacts from repo

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  base: "/portfolio/",
 }));
